be31581eec186b826fbbb8f2a3ca5eae8da5878f
abhimaurya <arm20493@gmail.com>
2019-12-28 19:51:48 +0530

    Updates

---
+++ precache-manifest.5c1234616da04bd4ca72d2a1001463e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b743c257ce5372d87aa221a517ef55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bd74ba6b85779c6a82b",
+    "url": "/static/css/main.816c4572.chunk.css"
+  },
+  {
+    "revision": "8912178a18e03af56c17",
+    "url": "/static/js/2.d702ab13.chunk.js"
+  },
+  {
+    "revision": "5599c98b7af73b31fed6a721c4f935e7",
+    "url": "/static/js/2.d702ab13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bd74ba6b85779c6a82b",
+    "url": "/static/js/main.de221f67.chunk.js"
+  },
+  {
+    "revision": "61b7e86fc5a1de2b24b2",
+    "url": "/static/js/runtime-main.5d0f8425.js"
+  }
+]);
